77520f19210edd01063f41fec05849405fa005f6
fjosescarmona <fjosescarmona@gmail.com>
2019-12-10 17:32:59 -0500

    primera carga servicio gestion informacion

---
+++ src/main/java/com/everis/bc/servicioGestionInformacion/ServicioGestionInformacionApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioGestionInformacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioGestionInformacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioGestionInformacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/bc/servicioGestionInformacion/ServicioGestionInformacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bc.servicioGestionInformacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioGestionInformacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
