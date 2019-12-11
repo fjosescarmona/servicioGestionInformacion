d4d9c7ea5b0b292e2d9b2af1df1ce054dbc816df
fjosescarmona <fjosescarmona@gmail.com>
2019-12-11 18:59:54 -0500

    agregando cambios

--- src/main/java/com/everis/bc/servicioGestionInformacion/ServicioGestionInformacionApplication.java
+++ src/main/java/com/everis/bc/servicioGestionInformacion/ServicioGestionInformacionApplication.java
@@ -2,7 +2,8 @@ package com.everis.bc.servicioGestionInformacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class ServicioGestionInformacionApplication {
 
